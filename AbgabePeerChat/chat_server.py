2a5f4dd32ebe06d8aba1587b90058ad0b957ff41
Pascal-2 <pascal.kaimer@t-online.de>
2025-05-22 12:38:33 +0200

    finished implementation of chat_server according to our protocol

--- AbgabePeerChat/chat_server.py
+++ AbgabePeerChat/chat_server.py
@@ -1,4 +1,3 @@
-import struct
 import threading
 from socket import socket
 
@@ -13,6 +12,7 @@ class Client:
 
 class Server:
     def __init__(self):
+        self.lock_clients_list = threading.Lock()
         self.clients = []
         self.id = 0
         self.ip = '127.0.0.1'
@@ -21,14 +21,45 @@ class Server:
         self.server_socket.bind((self.ip, self.port))
         self.server_socket.listen()
 
-    def register_client(self, ip, nickname, udp_port):
-        self.clients.append((ip, nickname, udp_port))
+    def notify_clients(self, mes_id, new_client):
+        with self.lock_clients_list:
+            for client in self.clients:
+                if len(client.nickname) == 0:
+                    continue
+                if client._id == new_client._id:
+                    #Peerliste
+                    if mes_id == 1:
+                        continue
+                    message = []
+                    for x in self.clients:
+                        if len(x.nickname) == 0:
+                            continue
+                        message.append([x.nickname, x.ip, x.udp_port])
+                    message = "\n".join(["|".join(y) for y in message])
+                    message = message.encode("utf-8")
+                    mes_len = len(message).to_bytes(4, byteorder='big')
+                    mes_id = (3).to_bytes(1, byteorder='big')
+                    outgoing_bytes = mes_len + mes_id + message
+                    new_client.socket.sendall(outgoing_bytes)
+                else:
+                    #Update an Peer: neue Anmeldung
+                    combined_str = new_client.nickname + "|" + new_client.ip + "|" + new_client.udp_port
+                    body = combined_str.encode('utf-8')
+                    mes_len = len(body).to_bytes(4, byteorder='big')
+                    mes_id = (mes_id).to_bytes(1, byteorder='big')
+                    outgoing_bytes = mes_len + mes_id + body
+                    client.socket.sendall(outgoing_bytes)
 
-    def send_update(self):
-        pass
+    def reject_nickname(self, client):
+        mes_len = (0).to_bytes(4, byteorder='big')
+        mes_id = (5).to_bytes(1, byteorder='big')
+        client.socket.sendall(mes_len + mes_id)
+        # Verbindung bleibt, oder beenden? Protokollabsprache!
 
     def send_bad_format(self, client_socket):
-        pass
+        mes_len = (0).to_bytes(4, byteorder='big')
+        mes_id = (6).to_bytes(1, byteorder='big')
+        client_socket.sendall(mes_len + mes_id)
 
     def send_broadcast(self, client, nickname, message):
         combined_str = nickname + "|" + message
@@ -48,25 +79,44 @@ class Server:
                 if not message:
                     break
                 local_client = None
-                for x in self.clients:
-                    if x.socket == client_socket:
-                        local_client = x
-                        break
+                with self.lock_clients_list:
+                    for x in self.clients:
+                        if x.socket == client_socket and x.addr == addr:
+                            local_client = x
+                            break
                 if message_id == 0:
                     #Anmeldung
                     splits = message.split("|")
-                    local_client.nickname = splits[0]
-                    local_client.ip = splits[1]
-                    local_client.udp_port = splits[2]
+                    if len(splits) != 3:
+                        self.send_bad_format(client_socket)
+                        break
+                    if len(splits[0]) > 20 or len(splits[0]) < 1:
+                        self.reject_nickname(local_client)
+                        continue
+                    with self.lock_clients_list:
+                        for x in self.clients:
+                            if x.nickname == splits[0]:
+                                self.reject_nickname(x)
+                                continue
+                        local_client.nickname = splits[0]
+                        local_client.ip = splits[1]
+                        local_client.udp_port = splits[2]
+                    self.notify_clients(0, local_client)
                 elif message_id == 1:
                     #Abmeldung
-                    self.clients.remove(local_client)
+                    with self.lock_clients_list:
+                        self.clients.remove(local_client)
+                    self.notify_clients(1, local_client)
+                    break
                 elif message_id == 2:
                     #Broadcast
-                    for x in self.clients:
-                        if x != local_client:
-                            self.send_broadcast(x, local_client.nickname, message)
-
+                    with self.lock_clients_list:
+                        for x in self.clients:
+                            if x != local_client:
+                                self.send_broadcast(x, local_client.nickname, message)
+                else:
+                    self.send_bad_format(client_socket)
+                    break
 
                 print(f"Received from {addr}: {message}")
                 client_socket.sendall(f"Echo: {message}".encode())
@@ -76,6 +126,14 @@ class Server:
                 self.send_bad_format(client_socket)
                 break
         client_socket.close()
+        # Abmeldung wenn Verbindung geschlossen wird
+        local_client = None
+        with self.lock_clients_list:
+            for x in self.clients:
+                if x.socket == client_socket and x.addr == addr:
+                    local_client = x
+            self.clients.remove(local_client)
+        self.notify_clients(1, local_client)
 
     """
     0	Anmeldung
@@ -88,8 +146,9 @@ class Server:
 
     def start_server(self):
         while True:
-            client_socket, addr, = self.server_socket.accept()
-            self.clients.append(Client(self.id, client_socket, addr))
+            client_socket, addr = self.server_socket.accept()
+            with self.lock_clients_list:
+                self.clients.append(Client(self.id, client_socket, addr))
             self.id += 1
             thread = threading.Thread(target=self.handle_client, args=(client_socket, addr))
             thread.start()
